5caf350aaab60b9e0e4be4b8169210be25207b3e
praveen choudhary <pc852180@gmail.com>
2025-03-06 18:01:59 +0530

    Vector

---
+++ vector.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+int main(){
+    vector<int>n;
+    n.push_back(2);
+    n.push_back(4);
+    n.push_back(5);
+    n.push_back(2);
+    n.push_back(1);
+    n.push_back(9);
+for(auto p:n){
+    cout << p << "\t";
+}
+}
+
+int n = nums.size();
+for(int i  =0;i<n;i++){
+    int count = 0; 
+    for(int  j = 0;)
+}
